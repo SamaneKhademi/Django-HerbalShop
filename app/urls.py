423a136a821727455fb8e3f7585e234ce520d3e9
Samane Khademi <s.khademi1992@gmail.com>
2024-07-19 00:24:26 +0330

    category view and product detail

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,7 @@ from django.conf import settings
 urlpatterns = [
     path('', views.home, name='home'),
     path('category/<slug:val>', views.CategoryView.as_view(), name='category'),
+    path('category-title/<val>', views.CategoryTitle.as_view(), name='category-title'),
+    path('product-detail/<int:pk>', views.ProductDetail.as_view(), name='product-detail'),
+    path('all-products/', views.AllProductsView.as_view(), name='all-products'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
