a83e03f3d271b5d1b44ebdb74a9a5aeb9352c027
Samane Khademi <s.khademi1992@gmail.com>
2024-07-22 00:25:26 +0330

    login

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_view
+from .forms import LoginForm
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -13,7 +15,8 @@ urlpatterns = [
     path('all-products/', views.AllProductsView.as_view(), name='all-products'),
 
     #login authentication
-    path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration')
+    path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
+    path('accounts/login/', auth_view.LoginView.as_view(template_name='app/login.html', authentication_form= LoginForm), name='login'),
 
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
