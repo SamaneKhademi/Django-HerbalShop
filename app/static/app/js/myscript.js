a62883d8922163c1296b19370b20e210c07de718
Samane Khademi <s.khademi1992@gmail.com>
2024-07-24 02:29:50 +0330

    minuscart and removecart completed

--- app/static/app/js/myscript.js
+++ app/static/app/js/myscript.js
@@ -18,3 +18,40 @@ $('.plus-cart').click(function (){
         }
     })
 })
+
+$('.minus-cart').click(function (){
+    var id=$(this).attr("pid").toString();
+    var eml = this.parentNode.children[2]
+    console.log('pid =',id)
+    $.ajax({
+        type:'GET',
+        url:'/minuscart',
+        data:{
+            prod_id:id
+        },
+        success:function (data){
+            console.log('data=',data);
+            eml.innerText=data.quantity
+            document.getElementById('amount').innerText=data.amount
+            document.getElementById('totalamount').innerText=data.totalamount
+        }
+    })
+})
+
+$('.remove-cart').click(function (){
+    var id=$(this).attr("pid").toString();
+    var eml = this
+    console.log('pid =',id)
+    $.ajax({
+        type:'GET',
+        url:'/removecart',
+        data:{
+            prod_id:id
+        },
+        success:function (data){
+            document.getElementById('amount').innerText=data.amount
+            document.getElementById('totalamount').innerText=data.totalamount
+            eml.parentNode.parentNode.parentNode.parentNode.remove()
+        }
+    })
+})
